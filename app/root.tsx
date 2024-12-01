a7c2822c7c376e2137a797f1542a9bd847fa747a
Jure Rotar <jrotar0@gmail.com>
2024-12-01 18:11:35 +0100

    chore: updated tailwindcss to v4 beta

--- app/root.tsx
+++ app/root.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 import { StateProvider } from './providers/state-provider';
-import './styles/styles.scss';
+import './styles/app.css';
 import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
